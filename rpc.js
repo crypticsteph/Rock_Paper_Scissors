0efb70fa98af462a4c2afa200a573b176c1750c6
crypticsteph <crypticsteph@gmail.com>
2024-02-06 11:28:34 -0500

    game function created

--- rpc.js
+++ rpc.js
@@ -1,13 +1,31 @@
-const rpc = ["rock","paper","scissors"];
+let rps = ["rock","paper","scissors"];
 
-function computerSelection(){
-const computer = rpc[ Math.floor(Math.random()*rpc.length)];
+function getComputerChoice() {
+let computer = rps[ Math.floor(Math.random()*rpc.length)];
 return  computer;
 }
 
-function playerSelection(){
-    let input = prompt("Type: Rock, Paper or Scissors");
-    input = input.toLocaleLowerCase();
+function round(playerSelection, computerSelection){
+if(playerSelection == computerSelection) {
+    return 'Tie Game';
+} else if(
+    (playerSelection == "rock" && computerSelection == "scissors") || 
+    (playerSelection == "scissors" && computerSelection == "paper") || 
+    (playerSelection == "paper" && computerSelection == "rock")  
+) {
+    return "Player Wins Match";
+
+} else{
+    return "Computer Wins Match"; 
+}
+}
+function game() {
+    for (let i = 0; i < 5; i++) {
     
+        let playerSelection = prompt("Select: rock, paper or scissors");
+        let computerSelection = getComputerChoice();
+        console.log(round(playerSelection, computerSelection));
+    }
 }
 
+game();
